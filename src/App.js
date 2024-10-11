21a35485229c741a05682025e2e4dd72e07081ae
JessCardenas <jesscardenasbcn@gmail.com>
2024-10-11 12:37:50 +0200

    added bootstrap to the project

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">Welcome</div>;
+  return (
+    <div className="App">
+      <div className="container">
+        Welcome
+        <footer>
+          This project was coded by{" "}
+          <a href="https://github.com/JessCardenas" target="blank">
+            Jessica Cárdenas
+          </a>{" "}
+          and is open-sourced on{" "}
+          <a
+            href="https://github.com/JessCardenas/react-weather-project"
+            target="blank"
+          >
+            GitHub
+          </a>
+        </footer>
+      </div>
+    </div>
+  );
 }
 
 export default App;

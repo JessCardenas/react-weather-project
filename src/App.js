1f97e77a1145c12dc6d03c2ea922e487ea6472b8
JessCardenas <jesscardenasbcn@gmail.com>
2024-11-03 10:23:01 +0100

    Added the correct icon URL

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <div className="container">
-        <Weather defaultCity="New York" />
+        <Weather defaultCity="Barcelona" />
         <footer>
           This project was coded by{" "}
           <a href="https://github.com/JessCardenas" target="blank">

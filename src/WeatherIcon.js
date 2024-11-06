71d00e9ca4dac2a6d05540e95b9ece2f68e354e0
JessCardenas <jesscardenasbcn@gmail.com>
2024-11-06 15:53:55 +0100

    added css styling

--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -25,7 +25,7 @@ export default function WeatherIcon(props) {
   return (
     <ReactAnimatedWeather
       icon={codeMapping[props.code]}
-      color="blue"
+      color="#66bfbf"
       size={props.size}
       animate={true}
     />
